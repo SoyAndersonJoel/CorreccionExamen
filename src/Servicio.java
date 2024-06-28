ceb0845e2a148a8acbe2bf838c709886ee3857a4
AndersonV123 <anderson.vilatuna@epn.edu.ec>
2024-06-27 23:09:39 -0500

    implementacion de ticket y servicio

--- src/Servicio.java
+++ src/Servicio.java
@@ -1,2 +1,13 @@
-public class Servicio {
+
+
+public abstract class Servicio {
+    protected String tipo;
+    protected String detalles;
+
+    public Servicio(String tipo, String detalles) {
+        this.tipo = tipo;
+        this.detalles = detalles;
+    }
+
+    public abstract double calcularFactorRecargo();
 }
--- src/Vip.java
+++ src/Vip.java
@@ -1,2 +1,20 @@
-public class Vip {
+import java.util.ArrayList;
+
+public class Vip extends Servicio {
+    private String tipoAsiento;
+    private int espacioMaleta;
+    private boolean maletaAdicional;
+    private ArrayList<String> serviciosAdicionales;
+
+    public Vip(String tipo, String detalles, String tipoAsiento, int espacioMaleta, boolean maletaAdicional, ArrayList<String> serviciosAdicionales) {
+        super(tipo, detalles);
+        this.tipoAsiento = tipoAsiento;
+        this.espacioMaleta = espacioMaleta;
+        this.maletaAdicional = maletaAdicional;
+        this.serviciosAdicionales = serviciosAdicionales;
+    }
+
+    public double calcularFactorRecargo() {
+        return 1.3;
+    }
 }
